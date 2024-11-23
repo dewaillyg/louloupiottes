66cf2d04467bb33cb32caecb2fe87212fc441f7c
guillaume dewailly <dewailly.freelance@gmail.com>
2024-11-23 17:25:47 +0100

    add login page link for desktop version

--- header.php
+++ header.php
@@ -49,6 +49,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['custom_login'])) {
                     'container' => 'ul',
                 ));
                 ?>
+                <a href="<?php $loginPage = get_page_by_path('connexion');
+                if ($loginPage) {
+                    $loginPageURL = get_permalink($loginPage->ID);
+                    echo esc_url($loginPageURL);
+                }?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                    <p>Connexion</p>
+                </a>
             </div>
         </div>
         <nav class="header__bottom">
@@ -66,7 +76,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['custom_login'])) {
             <div>
                 <?php get_search_form() ?>
             </div>
-            
+
             <div class="pages">
                 <h2>Pages</h2>
                 <div class="top__right">
---
+++ templates/login.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  Template Name: Login
+*/
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo("charset"); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <?php wp_body_open(); ?>
+    <h1>Connexion</h1>
+</body>
+</html>
